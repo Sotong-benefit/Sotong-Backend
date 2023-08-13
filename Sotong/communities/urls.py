99cabb7c34ac6f128b911c198cacb7ec075cd912
xogk1128 <xogk1128@naver.com>
2023-08-13 22:02:40 +0900

    즐겨찾기 ajax 반영

--- Sotong/communities/urls.py
+++ Sotong/communities/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from .views import post_list_view, post_create_form_view, post_detail_view, post_delete_view, post_update_view
+from .views import post_list_view, post_create_form_view, post_detail_view, post_delete_view, post_update_view, post_like_view
 
 app_name = 'community'
 
@@ -8,6 +8,10 @@ urlpatterns = [
     path('', post_list_view, name='post-list'),
     path('new/', post_create_form_view, name='post-create'),
     path('<int:id>/', post_detail_view, name='post-detail'),
-    path('<int:id>/delete', post_delete_view, name="post-delete"),
+    path('<int:id>/delete/', post_delete_view, name="post-delete"),
     path('<int:id>/edit/', post_update_view, name='post-update'),
+
+    # path('<int:id>/likes/', post_like_view, name='post-like'),
+
+    path('likes/', post_like_view, name='post-like'),
 ]
--- Sotong/communities/views.py
+++ Sotong/communities/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
-from .models import Community
+from .models import Community, Favorite
 from .forms import CommunityCreateForm
-
+from django.http import HttpResponse,JsonResponse
+import json
 
 # Create your views here.
 
@@ -13,6 +14,9 @@ def index(request):
 
 def post_list_view(request):
     post_list = Community.objects.all().order_by('-created_at')
+    if request.user.is_authenticated:
+        for post in post_list:
+            post.is_liked = post.favorite_set.filter(user=request.user).exists()
     context ={
         'post_list': post_list
     }
@@ -97,3 +101,84 @@ def post_delete_view(request, id):
             post.delete()
     return redirect('community:post-list')
     
+
+# @login_required
+# def post_like_view(request, id):
+#     if request.user.is_authenticated:
+#         community = get_object_or_404(Community, id=id)
+#         user = request.user
+
+#     # 이미 좋아요를 눌렀는지 확인
+#         if Favorite.objects.filter(user=user, community=community).exists():
+#             # 이미 좋아요를 눌렀을 경우 처리
+#             favorite = Favorite.objects.get(user=user, community=community)
+#             favorite.delete()
+
+#         else:
+#             # 중개 모델을 생성하고 저장
+#             like = Favorite(user=user, community=community)
+#             like.save()
+
+#         return redirect('community:post-list')
+        
+#     return redirect('accouts:login')
+
+# @login_required
+# def post_like_view(request, id):
+#     if request.method == 'GET' and request.user.is_authenticated:
+#         community = get_object_or_404(Community, id=id)
+#         user = request.user
+
+#     # 이미 좋아요를 눌렀는지 확인
+#         if Favorite.objects.filter(user=user, community=community).exists():
+#             # 이미 좋아요를 눌렀을 경우 처리
+#             favorite = Favorite.objects.get(user=user, community=community)
+#             favorite.delete()
+#             is_favorite = False
+
+#         else:
+#             # 중개 모델을 생성하고 저장
+#             like = Favorite(user=user, community=community)
+#             like.save()
+#             is_favorite = True
+
+#         return render(request, 'community/main_frame.html')
+#     else:
+#         render(request, 'community/main_frame.html')
+
+
+def post_like_view(request):
+    if request.method == 'GET': #ajax 방식일 때 아래 코드 실행
+        post_id = request.GET.get('post_id') #좋아요를 누른 게시물id (blog_id)가지고 오기'
+        print(post_id)
+
+        # post = Community.objects.get(id=post_id) 
+        post = get_object_or_404(Community, id=post_id)
+        # if not request.user.is_authenticated: #버튼을 누른 유저가 비로그인 유저일 때
+        #     message = "로그인을 해주세요" #화면에 띄울 메세지 
+        #     context = {"message":message}
+        #     return HttpResponse(json.dumps(context), content_type='application/json')
+
+        user = request.user #request.user : 현재 로그인한 유저
+        #     # 이미 좋아요를 눌렀는지 확인
+
+        if Favorite.objects.filter(user=user, community=post).exists():
+            # 이미 좋아요를 눌렀을 경우 처리
+            favorite = Favorite.objects.get(user=user, community=post)
+            favorite.delete()
+            is_favorite = False
+
+        else:
+            # 중개 모델을 생성하고 저장
+            like = Favorite(user=user, community=post)
+            like.save()
+            is_favorite = True
+
+        post_list = Community.objects.all().order_by('-created_at')
+        if request.user.is_authenticated:
+            for post in post_list:
+                post.is_liked = post.favorite_set.filter(user=request.user).exists()
+        context ={
+            'post_list': post_list
+        }
+        return render(request, 'community/main_frame.html', context)
