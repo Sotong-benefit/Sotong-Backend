eecbc0cf2649dc57545bcaeebffeda705c9cc91f
xogk1128 <xogk1128@naver.com>
2023-08-18 18:28:14 +0900

    post

--- Sotong/findbenefit/admin.py
+++ Sotong/findbenefit/admin.py
@@ -5,7 +5,7 @@ from findbenefit.models import Benefit
 # Register your models here.
 @admin.register(Benefit)
 class CommunityModelAdmin(admin.ModelAdmin):
-    list_display = ('image', 'title', 'tag', 'user',)
+    list_display = ('id', 'image', 'title', 'tag', 'user',)
     search_fields = ('id',)
     
     actions = ['make_published']
